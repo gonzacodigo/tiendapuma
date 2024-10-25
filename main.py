79d1993a6f5f64337d71f0b32ffd54af4205d2fc
Gonzalo Arce <gonzarcee@gmail.com>
2024-10-25 00:23:28 -0300

    Se agrego la carpeta raiz

---
+++ main.py
@@ -0,0 +1,136 @@
+from concurrent.futures import ThreadPoolExecutor
+from flask import Flask, render_template, request, send_from_directory, jsonify
+from flask_cors import CORS
+import mercadopago
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import os
+
+app = Flask(__name__)
+CORS(app)  # Habilitar CORS para todas las rutas
+session = requests.Session()  # Reutilizar la sesión HTTP
+
+@app.route('/')
+def index():
+    return render_template('tienda.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
+# Inicializar Mercado Pago con tu Access Token
+sdk = mercadopago.SDK(
+    "APP_USR-6353271101230954-091923-0134b8d1466acc7784abaf1dbce6cc7d-411256757"
+)
+
+@app.route('/pago/<float:precio>', methods=['POST'])
+def crear_pago(precio):
+    try:
+        data = request.get_json()
+        item_title = data.get('title', 'Compra en Puma')
+
+        preference_data = {
+            "items": [
+                {
+                    "title": item_title,
+                    "quantity": 1,
+                    "currency_id": "ARS",
+                    "unit_price": precio
+                }
+            ],
+            "back_urls": {
+                "success": "https://tiendaadidas.pythonanywhere.com/",
+                "failure": "https://tiendaadidas.pythonanywhere.com/",
+                "pending": ""
+            },
+            "auto_return": "approved"
+        }
+
+        preference_response = sdk.preference().create(preference_data)
+
+        if preference_response.get("error"):
+            return jsonify({'error': 'Error al crear la preferencia de pago'}), 500
+
+        preference = preference_response["response"]
+        return jsonify({'init_point': preference['init_point']})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/puma', methods=['GET'])
+def obtener_productos_puma():
+    url = "https://ar.puma.com/outlet"
+
+    try:
+        response = session.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        app.logger.error(f"Error en la solicitud: {e}")
+        return jsonify({'error': 'No se pudo obtener los productos'}), 500
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    productos = soup.find_all('a', class_='chakra-link')
+    resultado = []
+
+    for producto in productos:
+        title = producto.find('p', class_="chakra-text css-15jo16m")
+        price = producto.find('b', class_="chakra-text css-0")
+        price_original = producto.find('s', class_="chakra-text css-dcu1hw")
+        price_porcentaje = producto.find('span', class_="chakra-badge css-1rv344z")
+        div_imagen = producto.find('div', class_="css-0")
+        imagen_url = None
+        link_href = producto['href'] if 'href' in producto.attrs else None
+        
+        if title and price:
+            # Limpiar el precio (remover el símbolo "$" y convertir a float)
+            price_text = price.text.strip().replace('$', '').replace(',', '')
+            price_float = float(price_text) if price_text else 0
+
+            # Sumar un porcentaje al precio (ej: 10%)
+            porcentaje_aumento = 10
+            nuevo_precio = price_float + \
+                (price_float * porcentaje_aumento / 100)
+            price = nuevo_precio * 1000
+
+        if div_imagen:
+            imagen = div_imagen.find('img', class_="chakra-image css-169s1qj")
+            if imagen:
+                imagen_url = imagen.get('src')
+
+        if link_href and not link_href.startswith('http'):
+            link_href = urljoin("https://ar.puma.com/outlet", link_href)
+
+        try:
+            response_producto = session.get(link_href)
+            response_producto.raise_for_status()
+        except requests.RequestException as e:
+            app.logger.error(f"Error en la solicitud al producto: {e}")
+            continue
+
+        soup_producto = BeautifulSoup(response_producto.text, 'html.parser')
+        productos_article = soup_producto.find_all('div', class_='css-10olt1g')
+
+        imagenes_producto = []
+        if productos_article:
+            for article in productos_article:
+                imgs = article.find_all('img', class_="chakra-image css-0")
+                imagenes_producto.extend([img['src'] for img in imgs if 'src' in img.attrs])
+                       
+
+        if title:
+            resultado.append({
+                'title': title.text.strip() if title else None,
+                'price': f"${price:,.2f}",
+                'price_porcentaje': price_porcentaje.text.strip() if price_porcentaje else None,
+                'price_original': price_original.text.strip() if price_original else None,
+                'img': imagen_url,
+                'imagenes_producto': imagenes_producto,
+                'link_href': link_href
+            })
+
+    return jsonify(resultado)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ static/tienda.js
@@ -0,0 +1,192 @@
+let url = "http://127.0.0.1:5000/puma"; // Cambiar el endpoint a productos
+let cargando = document.getElementById("cargando");
+
+// Función para ordenar los productos según su precio (de menor a mayor)
+function ordenarProductosPorPrecio(productos) {
+  return productos.sort((a, b) => {
+    let precioA = parseFloat(a.price.replace("$", "").replace(/\./g, "").replace(",", "."));
+    let precioB = parseFloat(b.price.replace("$", "").replace(/\./g, "").replace(",", "."));
+    return precioA - precioB;
+  });
+}
+
+function getProductos() {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok for productos");
+      }
+      return response.json();
+    })
+    .then(data => {
+      let productosOrdenados = ordenarProductosPorPrecio(data);
+      getProductosHTML(productosOrdenados);
+    })
+    .catch(error => {
+      console.error("Error al obtener los productos:", error);
+      if (cargando) {
+        cargando.textContent = "Error al cargar los productos.";
+      }
+    });
+}
+
+function getProductosHTML(productos) {
+  let divResultados = document.getElementById("productos");
+
+  if (!divResultados) {
+    console.error('El elemento con id "productos" no se encontró.');
+    return;
+  }
+
+  if (cargando) {
+    cargando.innerHTML = "";
+  }
+
+  divResultados.innerHTML = "";
+
+  // Usar un fragmento de documento para optimizar la inserción masiva en el DOM
+  let fragment = document.createDocumentFragment();
+
+  productos.forEach(producto => {
+    let productoDiv = document.createElement("div");
+    productoDiv.classList.add("producto");
+
+    let title = document.createElement("h2");
+    title.textContent = producto.title;
+
+    let description = document.createElement("p");
+    description.textContent = producto.description;
+
+    let price = document.createElement("b");
+    price.textContent = `Precio: ${producto.price}`;
+    price.classList.add("priceAumentado");
+
+    let priceOriginal = document.createElement("p");
+    priceOriginal.innerHTML = `Precio Original: <del>${producto.price_original}</del>`;
+
+    let pricePorcentaje = document.createElement("p");
+    pricePorcentaje.textContent = `Descuento: ${producto.price_porcentaje}`;
+
+    let imagenUrl = producto.img;
+    if (imagenUrl) {
+      let imagen = document.createElement("img");
+      imagen.src = imagenUrl;
+      imagen.alt = producto.title;
+      imagen.loading = "lazy"; // Lazy loading
+      productoDiv.appendChild(imagen);
+    }
+
+    let verImagenesButton = document.createElement("button");
+    verImagenesButton.textContent = "Ver imágenes";
+    verImagenesButton.classList.add("btn-ver-imagenes");
+
+    verImagenesButton.addEventListener("click", function () {
+      mostrarCarrusel(producto.imagenes_producto);
+    });
+
+    let pagarButton = document.createElement("button");
+    pagarButton.textContent = "Pagar";
+    pagarButton.classList.add("btn-pago");
+
+    pagarButton.addEventListener("click", function () {
+      let precioAumentadoFloat = parseFloat(
+        producto.price.replace("$", "").replace(/\./g, "").replace(",", ".")
+      );
+
+      fetch(`/pago/${precioAumentadoFloat}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: producto.title }),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Error al crear la preferencia de pago.");
+          }
+          return response.json();
+        })
+        .then(data => {
+          window.location.href = data.init_point;
+        })
+        .catch(error => {
+          console.error("Error al crear la preferencia de pago:", error);
+          alert("Hubo un error al intentar procesar el pago. Inténtalo nuevamente.");
+        });
+    });
+
+    // Botón de WhatsApp
+    let whatsappButton = document.createElement("button");
+    whatsappButton.textContent = "CONTACTAR POR WHATSAPP";
+    whatsappButton.classList.add("btn-whatsapp");
+
+    whatsappButton.addEventListener("click", function () {
+      document
+        .getElementById("whatsappModal")
+        .setAttribute("data-product-title", producto.title);
+
+      document.getElementById("whatsappModal").style.display = "block";
+    });
+
+    productoDiv.appendChild(title);
+    productoDiv.appendChild(price);
+    productoDiv.appendChild(priceOriginal);
+    productoDiv.appendChild(verImagenesButton);
+    productoDiv.appendChild(whatsappButton);
+    productoDiv.appendChild(pagarButton);
+
+    fragment.appendChild(productoDiv);
+  });
+
+  divResultados.appendChild(fragment);
+}
+
+// Función para mostrar el modal con el carrusel
+function mostrarCarrusel(imagenes) {
+  let modal = document.getElementById("modal");
+  let carruselDiv = document.getElementById("carrusel");
+
+  carruselDiv.innerHTML = "";
+
+  imagenes.forEach(imagenUrl => {
+    let imgElement = document.createElement("img");
+    imgElement.src = imagenUrl;
+    imgElement.loading = "lazy"; // Lazy loading para carrusel
+    carruselDiv.appendChild(imgElement);
+  });
+
+  modal.style.display = "block";
+}
+
+// Cerrar el modal de imágenes
+document.getElementById("closeModal").onclick = function () {
+  document.getElementById("modal").style.display = "none";
+};
+
+// Otros modales y eventos
+document.getElementById("closeWhatsappModal").onclick = function () {
+  document.getElementById("whatsappModal").style.display = "none";
+};
+
+// Enviar formulario de WhatsApp
+document.getElementById("whatsappForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let nombreCompleto = document.getElementById("nombre").value;
+  let telefono = document.getElementById("telefono").value;
+  let direccion = document.getElementById("direccion").value;
+
+  let productoTitle = document
+    .getElementById("whatsappModal")
+    .getAttribute("data-product-title");
+
+  let mensaje = `Hola, mi nombre es ${nombreCompleto}. Estoy interesado en el producto "${productoTitle}". Mis datos de contacto son:\nTeléfono: ${telefono}\nDirección: ${direccion}`;
+
+  let whatsappURL = `https://wa.me/5491128470107?text=${encodeURIComponent(mensaje)}`;
+  window.open(whatsappURL, "_blank");
+
+  document.getElementById("whatsappModal").style.display = "none";
+});
+
+// Cargar productos al cargar la página
+document.addEventListener("DOMContentLoaded", getProductos);
